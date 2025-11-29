1702f74ab60579e03c2b9477f3920ffdead04e19
sanmu8 <1195286832@qq.com>
2025-11-30 01:10:36 +0800

    接口文档改为使用knife4j

--- src/main/java/com/zxh/common/config/SwaggerConfig.java
+++ src/main/java/com/zxh/common/config/SwaggerConfig.java
@@ -5,37 +5,70 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @EnableSwagger2
+// 添加Knife4j注解
 public class SwaggerConfig {
+
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo()) // 用于生成API信息
-                .select() // select()函数返回一个ApiSelectorBuilder实例,用来控制接口被swagger做成文档
+                .apiInfo(apiInfo())
+                .select()
                 .apis(RequestHandlerSelectors.basePackage("com.zxh"))
-                .paths(PathSelectors.any()
-                )
-                .build();
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
     }
 
     private ApiInfo apiInfo() {
-
-        Contact contact = new Contact(
-                "zxh", // 作者姓名
-                "zxh", // 作者网址
-                "zxh@qq.com"); // 作者邮箱
-
+        Contact contact = new Contact("zxh", "zxh", "zxh@qq.com");
         return new ApiInfoBuilder()
-                .title("zxh项目API") //  可以用来自定义API的主标题
-                .description("zxh项目SwaggerAPI管理") // 可以用来描述整体的API
+                .title("自习室预约系统API")
+                .description("自习室预约系统接口文档<br/>" +
+                        "<strong>Token说明：</strong>在「请求参数」中设置Authorization头，值为：Bearer + 空格 + 您的token")
                 .contact(contact)
+                .version("1.0")
                 .build();
+    }
+
+    /**
+     * 设置认证方式
+     */
+    private List<SecurityScheme> securitySchemes() {
+        List<SecurityScheme> securitySchemes = new ArrayList<>();
+        securitySchemes.add(new ApiKey("Authorization", "Authorization", "header"));
+        return securitySchemes;
+    }
+
+    /**
+     * 设置需要认证的路径
+     */
+    private List<SecurityContext> securityContexts() {
+        List<SecurityContext> securityContexts = new ArrayList<>();
+        securityContexts.add(SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build());
+        return securityContexts;
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        List<SecurityReference> securityReferences = new ArrayList<>();
+        securityReferences.add(new SecurityReference("Authorization", authorizationScopes));
+        return securityReferences;
     }
 }
